041d327c1f406a462dce368f8bd19668feb1595f
Simon T. <simtho0624@gmail.com>
2024-10-05 18:57:00 +0200

    Add a .env file for login DB

--- src/main/java/org/example/WebSite/Javascript/nodeServer/app.js
+++ src/main/java/org/example/WebSite/Javascript/nodeServer/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 const mysql = require('mysql2');
 
-
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'developer',
-    password: 'password',
-    database: 'WorkSql'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
